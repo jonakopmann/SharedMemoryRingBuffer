fd22210ba8a95128df2ce7b64a9b7ac27410a2b8
jonakopmann <uftak@student.kit.edu>
2023-03-21 13:17:42 +0100

    Changes lock to be pthread_rwlock_t

--- include/shmrb/ring-buffer.h
+++ include/shmrb/ring-buffer.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "lock.h"
+#include <glib-2.0/glib.h>
+#include <pthread.h>
 
 #define EVENT_BUFFER_SHM "/shm_ring_buffer"
 
@@ -14,12 +15,16 @@ private:
         gint end;
     };
     Header* m_header;
-    Lock* m_lock;
+    pthread_rwlock_t* m_lock;
     gpointer m_data;
 
     gsize m_itemSize;
     gsize m_size;
     gboolean m_isProducer;
+
+
+    void pthread_rwlock_init_pshared(pthread_rwlock_t* rwlock);
+
 public:
     RingBuffer(const gchar* name, gboolean isProducer, gsize capacity, gsize itemSize);
     ~RingBuffer();
@@ -32,10 +37,10 @@ public:
     gpointer GetWritePointer();
     gpointer GetReadPointer(gint& outCurrentIndex, gint desiredIndex = -1);
 
-    void LockRead() { m_lock->LockReader(); }
-    void UnlockRead() { m_lock->UnlockReader(); }
-    void LockWrite() { m_lock->LockWriter(); }
-    void UnlockWrite() { m_lock->UnlockWriter(); }
+    void LockRead() { pthread_rwlock_rdlock(m_lock); }
+    void UnlockRead() { pthread_rwlock_unlock(m_lock); }
+    void LockWrite() { pthread_rwlock_wrlock(m_lock); }
+    void UnlockWrite() { pthread_rwlock_unlock(m_lock); }
 
     static void Destroy(const gchar* name);
 };
